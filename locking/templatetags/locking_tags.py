6438f6aea471f7a9c94e7c5b1057675b6de33988
Souen Boniface <souenboniface@gmail.com>
2012-01-26 12:14:31 +0100

    DRYify

--- locking/templatetags/locking_tags.py
+++ locking/templatetags/locking_tags.py
@@ -33,7 +33,7 @@ def locking_variables(context):
             "applies": original.lock_applies_to(request.user),
             "change_form_id": "%s_form" % (original._meta.module_name,),
             "was_already_locked_by_user": getattr(original, '_was_already_locked_by_user', False),
-            "is_POST_response": request.method == 'POST',
+            "is_POST_response": is_POST_response,
             "error_when_saving": None
         }
         # If we are responding after a POST, export locking errors if any
