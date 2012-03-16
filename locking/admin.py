6ced1630bab7a185c001f40ea6ece0d491c4a433
Souen Boniface <souenboniface@gmail.com>
2012-03-17 01:25:25 +0800

    Use django.utils.simplejson instead of importing directly simplejson

--- locking/admin.py
+++ locking/admin.py
@@ -1,5 +1,5 @@
 # coding=utf8
-import simplejson
+from django.utils import simplejson
 from datetime import datetime
 from django.conf.urls.defaults import patterns, url
 from django.contrib import admin
